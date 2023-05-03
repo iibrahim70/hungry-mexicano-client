7b86bfb4904485e30af1e04e9a84b7c641171240
iibrahim70 <aviknuhash70@gmail.com>
2023-05-03 22:37:15 +0600

    added errorpage

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,11 +3,13 @@ import Main from "../components/layout/Main";
 import Home from "../components/pages/home/Home";
 import Login from "../components/pages/Login/Login";
 import Register from "../components/pages/Login/Register";
+import ErrorPage from "../components/pages/errorpage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: '/',
