29fb6433d1f63f19740dea7d3717300b93a0bc5d
iibrahim70 <iibrahiim.dev@gmail.com>
2023-05-03 12:15:15 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7kRe1H5RWw-5luk_XzZUp-XNqohUiMms",
+  authDomain: "hungry-mexicano.firebaseapp.com",
+  projectId: "hungry-mexicano",
+  storageBucket: "hungry-mexicano.appspot.com",
+  messagingSenderId: "987370084229",
+  appId: "1:987370084229:web:c4e3d2821a4e23b14ad6ec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
