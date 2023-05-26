4f685fa8ed731be4af454c7f72ab186d7a24478f
iibrahim70 <iibrahiim.dev@gmail.com>
2023-05-26 18:55:54 +0600

    update the recipe details ui

--- src/components/homepage/RecipeDetails.jsx
+++ src/components/homepage/RecipeDetails.jsx
@@ -1,73 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import RecipeDetailsCard from './RecipeDetailsCard';
 
 const RecipeDetails = () => {
-  const recipeDetails = useLoaderData(); 
-  
-  const [isFavorited1, setIsFavorited1] = useState(false);
-  const [isFavorited2, setIsFavorited2] = useState(false);
-  const [isFavorited3, setIsFavorited3] = useState(false);
-
-  const handleFavoriteClick1 = () => {
-    setIsFavorited1(true);
-    toast.error('This recipe is now your favorite!');
-  };
-  const handleFavoriteClick2 = () => {
-    setIsFavorited2(true);
-    toast.success('This recipe is now your favorite!');
-  };
-  const handleFavoriteClick3 = () => {
-    setIsFavorited3(true);
-    toast.warning('This recipe is now your favorite!');
-  };
+  const recipeDetails = useLoaderData();
 
   return (
-    <div>
-      <ToastContainer />
-      <LazyLoadImage className='w-full h-screen object-fill px-5 py-10' src={recipeDetails.chefPicture} alt="chef images" />
-      <div className='mx-5'>
-          <h2 className='text-2xl font-bold pb-1'>{recipeDetails.chefName}</h2>
+    <>
+      <LazyLoadImage className='w-full py-10' src={recipeDetails.chefPicture} alt="chef images" />
+      
+      <div>
+        <h2 className='text-2xl font-bold pb-1'>{recipeDetails.chefName}</h2>
         <p className='text-xl pb-1'><span className='font-semibold'>Short Bio:</span> {recipeDetails.bio}</p>
         <p className='text-xl pb-1'><span className='font-semibold'>Years of experience:</span> {recipeDetails.yearsOfExperience}</p>
         <p className='text-xl pb-1'><span className='font-semibold'>Numbers of recipes:</span> {recipeDetails.numberOfRecipes}</p>
         <p className='text-xl pb-1'><span className='font-semibold'>Likes:</span> {recipeDetails.likes.facebook}</p>
       </div>
-      <div className='flex justify-evenly mx-5 my-10'>
-        <div className="card w-80 bg-base-100 shadow-xl mb-5">
-          <div className="card-body">
-            <h2 className='text-2xl font-bold'>{recipeDetails.recipe_one.recipe_name1}</h2>
-            <p className='text-base font-semibold'>Ingredients -</p>
-            {recipeDetails?.recipe_one?.ingredients?.map((item, index) => <li key={index}>{item}</li>)}
-            <p><span className='text-base font-semibold'>Cooking Method: </span>{recipeDetails.recipe_one.cooking_method1}</p>
-            <p><span className='text-base font-semibold'>Ratings: </span>{recipeDetails.recipe_one.rating1}</p>
-            <button onClick={handleFavoriteClick1} disabled={isFavorited1} className="btn btn-primary">Favourite</button>
-          </div>
-        </div>
-        <div className="card w-80 bg-base-100 shadow-xl mb-5">
-          <div className="card-body">
-            <h2 className='text-2xl font-bold'>{recipeDetails.recipe_two.recipe_name2}</h2>
-            <p className='text-base font-semibold'>Ingredients -</p>
-            {recipeDetails?.recipe_two?.ingredients?.map((item, index) => <li key={index}>{item}</li>)}
-            <p><span className='text-base font-semibold'>Cooking Method: </span>{recipeDetails.recipe_two.cooking_method2}</p>
-            <p><span className='text-base font-semibold'>Ratings: </span>{recipeDetails.recipe_two.rating2}</p>
-            <button onClick={handleFavoriteClick2} disabled={isFavorited2} className="btn btn-primary">Favourite</button>
-          </div>
-        </div>
-        <div className="card w-80 bg-base-100 shadow-xl mb-5">
-          <div className="card-body">
-            <h2 className='text-2xl font-bold'>{recipeDetails.recipe_three.recipe_name3}</h2>
-            <p className='text-base font-semibold'>Ingredients -</p>
-            {recipeDetails?.recipe_three?.ingredients?.map((item, index) => <li key={index}>{item}</li>)}
-            <p><span className='text-base font-semibold'>Cooking Method: </span>{recipeDetails.recipe_three.cooking_method3}</p>
-            <p><span className='text-base font-semibold'>Ratings: </span>{recipeDetails.recipe_three.rating3}</p>
-            <button onClick={handleFavoriteClick3} disabled={isFavorited3} className="btn btn-primary">Favourite</button>
-          </div>
-        </div>
+      
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10 py-10'>
+        {recipeDetails?.recipes.map((recipe, index) => <RecipeDetailsCard key={index} recipe={recipe} />)}
       </div>
-    </div>
+
+    </>
   );
 };
 
---
+++ src/components/homepage/RecipeDetailsCard.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+
+const RecipeDetailsCard = ({ recipe }) => {
+  const {recipe_name, cooking_method, rating} = recipe;
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  const handleFavoriteClick = () => {
+    setIsFavorited(true);
+    toast.success('This recipe is now your favorite!');
+  };
+
+  return (
+    <div className='ps-5 space-y-1 pb-2 shadow-2xl'>
+      <ToastContainer />
+      <h2 className='text-base font-semibold pt-2'>{recipe_name}</h2>
+      <p className='text-base font-semibold'>Ingredients -</p>
+      {recipe.ingredients?.map((item, index) => <li className='capitalize ps-3' key={index}>{item}</li>)}
+      <p><span className='text-base font-semibold'>Cooking Method: </span>{cooking_method}</p>
+      <p><span className='text-base font-semibold'>Rating: </span>{rating}</p>
+      <button onClick={handleFavoriteClick} disabled={isFavorited} className="inline-flex items-center justify-center capitalize text-sm px-3 py-2 font-normal text-white transition hover:drop-shadow-2xl rounded border-none shadow-xl md:mb-0 bg-blue-600 hover:bg-blue-800 duration-700 disabled:cursor-not-allowed disabled:bg-slate-400">Favourite</button>
+    </div>
+  );
+};
+
+export default RecipeDetailsCard;
