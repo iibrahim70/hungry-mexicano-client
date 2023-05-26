f8ed5f756d8a4d5778fb089553af02aec540555e
iibrahim70 <aviknuhash70@gmail.com>
2023-05-26 20:37:49 +0600

    change disable button color

--- src/components/homepage/RecipeDetailsCard.jsx
+++ src/components/homepage/RecipeDetailsCard.jsx
@@ -11,13 +11,15 @@ const RecipeDetailsCard = ({ recipe }) => {
   };
 
   return (
-    <div className='ps-5 space-y-1 pb-2 shadow-2xl'>
+    <div className='px-5 space-y-2 shadow-2xl flex flex-col'>
       <ToastContainer />
-      <h2 className='text-base font-semibold pt-2'>{recipe_name}</h2>
-      <p className='text-base font-semibold'>Ingredients -</p>
-      {recipe.ingredients?.map((item, index) => <li className='capitalize ps-3' key={index}>{item}</li>)}
-      <p><span className='text-base font-semibold'>Cooking Method: </span>{cooking_method}</p>
-      <p><span className='text-base font-semibold'>Rating: </span>{rating}</p>
+      <div className='flex-1'>
+        <h2 className='text-base font-semibold pt-2'>{recipe_name}</h2>
+        <p className='text-base font-semibold'>Ingredients -</p>
+        {recipe.ingredients?.map((item, index) => <li className='capitalize ps-3' key={index}>{item}</li>)}
+        <p className='text-justify'><span className='text-base font-semibold'>Cooking Method: </span>{cooking_method}</p>
+        <p><span className='text-base font-semibold'>Rating: </span>{rating}</p>
+      </div>
       <button onClick={handleFavoriteClick} disabled={isFavorited} className="inline-flex items-center justify-center capitalize text-sm px-3 py-2 font-normal text-white transition hover:drop-shadow-2xl rounded border-none shadow-xl md:mb-0 bg-blue-600 hover:bg-blue-800 duration-700 disabled:cursor-not-allowed disabled:bg-slate-400">Favourite</button>
     </div>
   );
